3e7e34fd3b4ff0339768cce7d815d78946a978b9
realk1ko <32820057+realk1ko@users.noreply.github.com>
2022-10-26 04:58:23 +0200

    Some fixes

--- container/home/app/run_app.sh
+++ container/home/app/run_app.sh
@@ -9,9 +9,9 @@ then
   # initialize database
   # FIXME sqlcmd actually never returns a non-zero status code (AFAIK), so the echos below will never be printed
   /opt/mssql-tools/bin/sqlcmd -S "${DATABASE_HOST}" -U SA -P "${DATABASE_PASSWORD}" -i "${HOME}/createUser.sql" || \
-    echo "User already exists."
+    echo 'User already exists.'
   /opt/mssql-tools/bin/sqlcmd -S "${DATABASE_HOST}" -U SA -P "${DATABASE_PASSWORD}" -i "${HOME}/createDB.sql" || \
-    echo "Database already exists."
+    echo 'Database already exists.'
 
   # set up ODBC configuration
   cat <<EOF > "${HOME}/odbc.ini"
@@ -25,7 +25,7 @@ EOF
 
   # finish installation of Bee-Up
   wine "C:/Program Files/BOC/BEEUP16_ADOxx_SA/adbinst.exe" -d"${DATABASE_NAME}" -l"${ADOXX_LICENSE_KEY}" -sSQLServer -iNO_SSO -lang2057 || \
-    echo "Installation failed. This installation is probably being upgraded."
+    echo 'Installation failed. This installation is probably being upgraded.'
 
   # disables the required password change
   /opt/mssql-tools/bin/sqlcmd -S "${DATABASE_HOST}" -U SA -P "${DATABASE_PASSWORD}" -i "${HOME}/postprocess.sql"
--- container/home/app/run_mssql.sh
+++ container/home/app/run_mssql.sh
@@ -3,7 +3,7 @@
 set -xe
 
 # only start if using the packaged MSSQL database
-if [ "${DATABASE_HOST}" = "127.0.0.1" ]
+if [ "${DATABASE_HOST}" = '127.0.0.1' ]
 then
-  SA_PASSWORD=${DATABASE_SA_PASSWORD} /opt/mssql/bin/sqlservr
+  MSSQL_SA_PASSWORD=${DATABASE_PASSWORD} /opt/mssql/bin/sqlservr
 fi
